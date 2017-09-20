09bf767627e1836e058b879063f6922e11340247
mandeep <mandeepsinghn@gmail.com>
2017-09-20 17:55:28 +0530

    First Step

---
+++ src/main/java/com/mentation/school/api/SchoolapiApplication.java
@@ -0,0 +1,12 @@
+package com.mentation.school.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolapiApplication.class, args);
+	}
+}
